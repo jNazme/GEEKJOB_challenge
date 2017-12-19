f97b0148c983cddb046552cb4decbcfa913b2529
fujita <mkuhoin@gmail.com>
2017-12-19 11:23:52 +0900

    PHPの課題　四則演算

---
+++ CampChallenge_PHP/003.四則演算/calc.php
@@ -0,0 +1,8 @@
+<?php
+const NUMBER1 = 88;
+$number2 = 55;
+echo NUMBER1 + $number2."<br>";
+echo NUMBER1 - $number2."<br>";
+echo NUMBER1 * $number2."<br>";
+echo NUMBER1 / $number2."<br>";
+echo NUMBER1 % $number2."<br>";
